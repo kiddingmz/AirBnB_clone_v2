df46acbbfdad83196552ecd7dec23f07f1ee8631
kiddingmz <bata.isidro@gmail.com>
2023-11-23 00:42:47 +0200

    CHORE: improves the city model

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(128), ForeignKey("states.id"), nullable=False)
+
+    state = relationship("State", back_populates="cities")
+
+    def __init__(self, *args, **kwargs):
+        filtered_kwargs = {k: v for k, v in kwargs.items() if hasattr(self, k) or k == "id"}
+        super().__init__(*args, **filtered_kwargs)
